9f70c24a9fa1adeae460a42658cf061f0020e7a5
Thorfinn-Karlsefniest <m.prottoy.siddique@gmail.com>
2025-03-20 00:41:52 -0400

    Initial File Commit

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 import mysql.connector
-from mysql.connector import Error
+#from mysql.connector import
 import pandas as pd
-import random
+import random as rd
+import discord
+import discord as disc
+from discord.ext import commands
 
 
 
